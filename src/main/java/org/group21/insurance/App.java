5ba6f19e57d6ad084b601bb941160afa5d5d418c
s3818206 <s3818206@rmit.edu.vn>
2024-04-25 13:44:30 +0700

    Create Login Screen Scene with function to print username and password to console, create the class implement in App class

--- src/main/java/org/group21/insurance/App.java
+++ src/main/java/org/group21/insurance/App.java
@@ -16,9 +16,10 @@ public class App extends Application {
     var javaVersion = SystemInfo.javaVersion();
     var javafxVersion = SystemInfo.javafxVersion();
 
-    var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    var scene = new Scene(new StackPane(label), 640, 480);
-    stage.setScene(scene);
+//    var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+//    var scene = new Scene(new StackPane(label), 1000, 800);
+//    stage.setTitle("Insurance Claim Management System");
+    stage.setScene(new LoginScreen().setLoginScreenUI(stage));
     stage.show();
   }
 
