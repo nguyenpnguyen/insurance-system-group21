db37462f88db324e446c1c9954e4fd8fe92b4b87
s3818206 <s3818206@rmit.edu.vn>
2024-04-27 19:24:27 +0700

    Change the LoginScreen and DashboardScreen back to not use fxml files to not getting error

--- src/main/java/org/group21/insurance/Views/DashboardScreen.java
+++ src/main/java/org/group21/insurance/Views/DashboardScreen.java
@@ -1,19 +1,35 @@
-// DashboardScreen.java
 package org.group21.insurance.Views;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class DashboardScreen extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label label = new Label("Welcome to the Dashboard!");
-        StackPane root = new StackPane();
-        root.getChildren().add(label);
+        BorderPane root = new BorderPane();
+
+        VBox sidebar = new VBox();
+        Button section1 = new Button("Section 1");
+        Button section2 = new Button("Section 2");
+        Button section3 = new Button("Section 3");
+        Button section4 = new Button("Section 4");
+        Button section5 = new Button("Section 5");
+        sidebar.getChildren().addAll(section1, section2, section3, section4, section5);
+
+        section1.setOnAction(e -> root.setCenter(new Label("Content for Section 1")));
+        section2.setOnAction(e -> root.setCenter(new Label("Content for Section 2")));
+        section3.setOnAction(e -> root.setCenter(new Label("Content for Section 3")));
+        section4.setOnAction(e -> root.setCenter(new Label("Content for Section 4")));
+        section5.setOnAction(e -> root.setCenter(new Label("Content for Section 5")));
+
+        root.setLeft(sidebar);
+
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setTitle("Dashboard");
         primaryStage.setScene(scene);
--- src/main/java/org/group21/insurance/Views/LoginScreen.java
+++ src/main/java/org/group21/insurance/Views/LoginScreen.java
@@ -1,34 +1,51 @@
 package org.group21.insurance.Views;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class LoginScreen extends Application {
-    @FXML
     private TextField nameInput;
-
-    @FXML
     private PasswordField passInput;
-
-    @FXML
     private Button loginButton;
 
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/group21/insurance/Views/LoginScreen.fxml"));
-        loader.setController(this);
-        stage.setScene(new Scene(loader.load()));
+    public void start(Stage stage) {
+        stage.setScene(setLoginScreenUI(stage));
         stage.setTitle("Insurance Claim Management System - Login");
         stage.show();
     }
 
-    @FXML
+    public Scene setLoginScreenUI(Stage stage) {
+        nameInput = new TextField();
+        passInput = new PasswordField();
+        loginButton = new Button("Login");
+
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(5);
+        gridPane.setVgap(5);
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+
+        gridPane.add(new Label("Insurance Claim Management System"), 0, 0, 2, 1);
+        gridPane.add(new Label("Username:"), 0, 1);
+        gridPane.add(nameInput, 1, 1);
+        gridPane.add(new Label("Password:"), 0, 2);
+        gridPane.add(passInput, 1, 2);
+        gridPane.add(loginButton, 1, 3);
+
+        loginButton.setOnAction(e -> handleLogin());
+
+        return new Scene(gridPane);
+    }
+
     private void handleLogin() {
         String username = nameInput.getText();
         String password = passInput.getText();
@@ -51,3 +68,53 @@ public class LoginScreen extends Application {
         launch(args);
     }
 }
+
+//package org.group21.insurance.Views;
+//
+//import javafx.application.Application;
+//import javafx.fxml.FXML;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.PasswordField;
+//import javafx.scene.control.TextField;
+//import javafx.stage.Stage;
+//
+//public class LoginScreen extends Application {
+//    private TextField nameInput;
+//
+//    private PasswordField passInput;
+//
+//    private Button loginButton;
+//
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/group21/insurance/Views/LoginScreen.fxml"));
+//        loader.setController(this);
+//        stage.setScene(new Scene(loader.load()));
+//        stage.setTitle("Insurance Claim Management System - Login");
+//        stage.show();
+//    }
+//
+//    private void handleLogin() {
+//        String username = nameInput.getText();
+//        String password = passInput.getText();
+//        if (username.isEmpty() || password.isEmpty()) {
+//            System.out.println("Error: Username and password must not be empty.");
+//        } else {
+//            // Handle login logic here
+//            System.out.println("Username: " + username);
+//            System.out.println("Password: " + password);
+//            DashboardScreen dashboardScreen = new DashboardScreen();
+//            try {
+//                dashboardScreen.start(new Stage());
+//            } catch (Exception ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
