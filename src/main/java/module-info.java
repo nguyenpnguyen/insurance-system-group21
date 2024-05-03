198cfdbe55053fbe93c7984e78e6fba605039fae
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-05-03 16:28:57 +0700

    fix: module-info fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.group21.insurance {
-    requires javafx.controls;
-    requires jakarta.persistence;
-    exports org.group21.insurance;
+  requires javafx.controls;
+  requires jakarta.persistence;
+  exports org.group21.insurance.models;
 	exports org.group21.insurance.views;
+	exports org.group21.insurance.daos;
 }
