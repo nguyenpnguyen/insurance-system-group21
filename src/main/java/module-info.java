2d5d13336d92841977d3eb2cc5a7c51d3ee4e75a
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-05-10 16:59:28 +0700

    fix: database connection and dependencies
    thank god it finally works

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module org.group21.insurance {
-    requires javafx.controls;
-    requires jakarta.persistence;
-    requires org.hibernate.orm.core;
+  requires javafx.controls;
+  requires jakarta.persistence;
+	requires org.hibernate.orm.core;
 	exports org.group21.insurance;
-    exports org.group21.insurance.views;
+  exports org.group21.insurance.views;
+	opens org.group21.insurance.models;
 }
