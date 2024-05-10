d78b289d5cccbcb3aa50924cf0c91288d58b39a8
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-05-10 14:19:36 +0700

    feat: add hibernate

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module org.group21.insurance {
     requires javafx.controls;
     requires jakarta.persistence;
-    requires org.eclipse.persistence.jpa;
+    requires org.hibernate.orm.core;
     exports org.group21.insurance;
     exports org.group21.insurance.views;
 }
