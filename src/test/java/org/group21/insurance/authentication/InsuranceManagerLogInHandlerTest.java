050b923afb89e5984f07447d48a95e65d120c294
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-05-15 03:23:10 +0700

    feat: add test for insurance manager log in

---
+++ src/test/java/org/group21/insurance/authentication/InsuranceManagerLogInHandlerTest.java
@@ -0,0 +1,64 @@
+package org.group21.insurance.authentication;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.group21.insurance.models.InsuranceProvider;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class InsuranceManagerLogInHandlerTest {
+	private static final String PERSISTENCE_UNIT_NAME = "insurance";
+	private static final String TEST_USERNAME = "insuranceManager";
+	private static final String TEST_PASSWORD = "testPassword";
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private static InsuranceManagerLogInHandler insuranceManagerLogInHandler;
+	
+	@BeforeAll
+	static void setUpAll() {
+		emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		em = emf.createEntityManager();
+	}
+	
+	@AfterAll
+	static void tearDownAll() {
+		em.close();
+		emf.close();
+	}
+	
+	@BeforeEach
+	void setUp() {
+		insuranceManagerLogInHandler = InsuranceManagerLogInHandler.getInstance(em);
+	}
+	
+	@Test
+	void isAuthenticated() {
+		PasswordAuthenticator passwordAuthenticator = new PasswordAuthenticator();
+		String testPassword = passwordAuthenticator.hash(TEST_PASSWORD.toCharArray());
+		
+		// Create an insurance manager entity
+		InsuranceProvider insuranceManager = new InsuranceProvider();
+		insuranceManager.setUsername(TEST_USERNAME);
+		insuranceManager.setHashedPassword(testPassword);
+		insuranceManager.setInsuranceManager(true);
+		
+		// Persist the insurance manager entity
+		em.getTransaction().begin();
+		em.persist(insuranceManager);
+		em.getTransaction().commit();
+		
+		boolean authenticated = insuranceManagerLogInHandler.isAuthenticated(TEST_USERNAME, TEST_PASSWORD);
+		
+		assertTrue(authenticated);
+		
+		// Clean up
+		em.getTransaction().begin();
+		em.remove(insuranceManager);
+		em.getTransaction().commit();
+	}
+}
