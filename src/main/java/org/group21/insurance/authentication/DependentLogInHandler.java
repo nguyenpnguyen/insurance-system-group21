e59c3e5199155e85310c70894aa1c87ee83bde22
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-05-14 23:42:18 +0700

    feat: implement dependent login

--- src/main/java/org/group21/insurance/authentication/DependentLogInHandler.java
+++ src/main/java/org/group21/insurance/authentication/DependentLogInHandler.java
@@ -1,15 +1,48 @@
 package org.group21.insurance.authentication;
 
+import jakarta.persistence.EntityManager;
+import org.group21.insurance.controllers.BeneficiaryController;
+import org.group21.insurance.models.Beneficiary;
+
+import java.util.Optional;
+
 public class DependentLogInHandler implements LogInHandler {
+	private static final String ROLE = "Dependent";
+	private static EntityManager em;
 	private static DependentLogInHandler instance = null;
+	
+	private DependentLogInHandler(BeneficiaryController beneficiaryController) {
+	}
+	
+	public static DependentLogInHandler getInstance(EntityManager entityManager) {
+		em = entityManager;
+		if (instance == null) {
+			instance = new DependentLogInHandler(BeneficiaryController.getInstance(em));
+		}
+		return instance;
+	}
 	
 	@Override
 	public boolean isAuthenticated(String username, String password) {
-		return false;
+		BeneficiaryController controller = BeneficiaryController.getInstance(em);
+		
+		Optional<Beneficiary> optionalDependent = controller.findByUsername(username);
+		if (optionalDependent.isEmpty()) {
+			return false;
+		}
+		
+		Beneficiary dependent = optionalDependent.get();
+		if (dependent.isPolicyHolder()) {
+			return false;
+		}
+		
+		PasswordAuthenticator passwordAuthenticator = new PasswordAuthenticator();
+		
+		return passwordAuthenticator.authenticate(password.toCharArray(), dependent.getHashedPassword());
 	}
 	
 	@Override
 	public String getRole() {
-		return null;
+		return ROLE;
 	}
 }
