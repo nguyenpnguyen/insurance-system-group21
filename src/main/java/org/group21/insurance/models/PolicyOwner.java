e4058252d458c1d2141db50e3745419a6fb499bf
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-05-14 17:15:36 +0700

    chore: cleanup unused import

--- src/main/java/org/group21/insurance/models/PolicyOwner.java
+++ src/main/java/org/group21/insurance/models/PolicyOwner.java
@@ -1,31 +1,33 @@
 package org.group21.insurance.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "PolicyOwner")
 @Table(name = "policy_owners")
 public class PolicyOwner extends Customer implements Serializable {
-  
-  @OneToMany(mappedBy = "policyOwner", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-  private List<Beneficiary> beneficiaries;
-
-  public PolicyOwner() {
-  }
-
-  public PolicyOwner(String id, String username, String hashedPassword, List<Beneficiary> beneficiaries) {
-    super(id, username, hashedPassword);
-    this.beneficiaries = beneficiaries;
-  }
-  
-  public List<Beneficiary> getBeneficiaries() {
-    return beneficiaries;
-  }
-  
-  public void setBeneficiaries(List<Beneficiary> beneficiaries) {
-    this.beneficiaries = beneficiaries;
-  }
+	
+	@OneToMany(mappedBy = "policyOwner", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	private List<Beneficiary> beneficiaries;
+	
+	public PolicyOwner() {
+	}
+	
+	public PolicyOwner(String id, String username, String hashedPassword, List<Beneficiary> beneficiaries) {
+		super(id, username, hashedPassword);
+		this.beneficiaries = beneficiaries;
+	}
+	
+	public List<Beneficiary> getBeneficiaries() {
+		return beneficiaries;
+	}
+	
+	public void setBeneficiaries(List<Beneficiary> beneficiaries) {
+		this.beneficiaries = beneficiaries;
+	}
 }
