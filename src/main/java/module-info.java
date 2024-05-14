46840a30559b284c96958de577c45f9fadacf0bb
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-05-14 16:13:39 +0700

    chore: configure dependencies and exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module org.group21.insurance {
-  requires javafx.controls;
-  requires jakarta.persistence;
+	requires javafx.controls;
+	requires jakarta.persistence;
 	requires org.hibernate.orm.core;
 	exports org.group21.insurance;
-  exports org.group21.insurance.views;
+	exports org.group21.insurance.views;
+	exports org.group21.insurance.controllers;
+	exports org.group21.insurance.utils;
 	opens org.group21.insurance.models;
 }
