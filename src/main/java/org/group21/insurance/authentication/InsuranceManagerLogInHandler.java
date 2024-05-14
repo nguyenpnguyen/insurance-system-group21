626ffed68cb7861453e6158a37a6bbce484c7c94
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-05-14 23:45:35 +0700

    feat: implement insurance manager login

--- src/main/java/org/group21/insurance/authentication/InsuranceManagerLogInHandler.java
+++ src/main/java/org/group21/insurance/authentication/InsuranceManagerLogInHandler.java
@@ -1,13 +1,48 @@
 package org.group21.insurance.authentication;
 
+import jakarta.persistence.EntityManager;
+import org.group21.insurance.controllers.InsuranceProviderController;
+import org.group21.insurance.models.InsuranceProvider;
+
+import java.util.Optional;
+
 public class InsuranceManagerLogInHandler implements LogInHandler {
+	private static final String ROLE = "InsuranceManager";
+	private static EntityManager em;
+	private static InsuranceManagerLogInHandler instance = null;
+	
+	private InsuranceManagerLogInHandler(InsuranceProviderController ipController) {
+	}
+	
+	public static InsuranceManagerLogInHandler getInstance(EntityManager entityManager) {
+		em = entityManager;
+		if (instance == null) {
+			instance = new InsuranceManagerLogInHandler(InsuranceProviderController.getInstance(em));
+		}
+		return instance;
+	}
+	
 	@Override
 	public boolean isAuthenticated(String username, String password) {
-		return false;
+		InsuranceProviderController controller = InsuranceProviderController.getInstance(em);
+		
+		Optional<InsuranceProvider> optionalIm = controller.findByUsername(username);
+		if (optionalIm.isEmpty()) {
+			return false;
+		}
+		
+		InsuranceProvider insuranceManager = optionalIm.get();
+		if (!insuranceManager.isInsuranceManager()) {
+			return false;
+		}
+		
+		PasswordAuthenticator passwordAuthenticator = new PasswordAuthenticator();
+		
+		return passwordAuthenticator.authenticate(password.toCharArray(), insuranceManager.getHashedPassword());
 	}
 	
 	@Override
 	public String getRole() {
-		return null;
+		return ROLE;
 	}
 }
