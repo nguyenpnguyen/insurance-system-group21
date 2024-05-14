ea9849e3e474b8390ecf02627111857cff60d713
Nguyen Phuc Nguyen <nguyen.pn@proton.me>
2024-05-15 03:01:40 +0700

    feat: add test for SystemAdminLogInHandler

---
+++ src/test/java/org/group21/insurance/authentication/SystemAdminLogInHandlerTest.java
@@ -0,0 +1,63 @@
+package org.group21.insurance.authentication;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.group21.insurance.models.SystemAdmin;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SystemAdminLogInHandlerTest {
+	private static final String PERSISTENCE_UNIT_NAME = "insurance";
+	private static final String TEST_USERNAME = "testAdmin";
+	private static final String TEST_PASSWORD = "testPassword";
+	private static EntityManagerFactory emf;
+	private static EntityManager em;
+	private static SystemAdminLogInHandler systemAdminLogInHandler;
+	
+	@BeforeAll
+	static void setUpAll() {
+		emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		em = emf.createEntityManager();
+	}
+	
+	@AfterAll
+	static void tearDownAll() {
+		em.close();
+		emf.close();
+	}
+	
+	@BeforeEach
+	void setUp() {
+		systemAdminLogInHandler = SystemAdminLogInHandler.getInstance(em);
+	}
+	
+	@Test
+	void isAuthenticated() {
+		PasswordAuthenticator passwordAuthenticator = new PasswordAuthenticator();
+		String testPassword = passwordAuthenticator.hash(TEST_PASSWORD.toCharArray());
+		
+		// Create a system admin entity
+		SystemAdmin systemAdmin = new SystemAdmin();
+		systemAdmin.setUsername(TEST_USERNAME);
+		systemAdmin.setHashedPassword(testPassword);
+		
+		// Persist the system admin entity
+		em.getTransaction().begin();
+		em.persist(systemAdmin);
+		em.getTransaction().commit();
+		
+		boolean authenticated = systemAdminLogInHandler.isAuthenticated(TEST_USERNAME, TEST_PASSWORD);
+		
+		assertTrue(authenticated);
+		
+		// Clean up
+		em.getTransaction().begin();
+		em.remove(systemAdmin);
+		em.getTransaction().commit();
+	}
+}
